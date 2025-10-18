7dd3c742d508842713fc4d899f6ca2aac614ecfa
ikrsx <krishal.sh@tuta.io>
2025-10-19 00:50:01 +0530

    removed gtk-themes

--- users/ikrsx/settings/dotfiles.nix
+++ users/ikrsx/settings/dotfiles.nix
@@ -2,12 +2,9 @@
 let
   dotfiles = "${config.home.homeDirectory}/Workstation/Development/krisnix/users/ikrsx/dotfiles";
   create_symlink = loc: config.lib.file.mkOutOfStoreSymlink loc;
-  theme = "Gruvbox-Green";
   configs = {
     nvim = "nvim";
     kitty = "kitty";
-    "gtk-4.0" = "gtk-themes/${theme}/gtk-4.0";
-    "gtk-3.0" = "gtk-themes/${theme}/gtk-3.0";
   };
 in 
 {
@@ -17,8 +14,4 @@ in
       recursive = true;
     })
     configs;
-
-  home.file = {
-    ".themes/".source = ../dotfiles/gtk-themes;
-  };
 }
