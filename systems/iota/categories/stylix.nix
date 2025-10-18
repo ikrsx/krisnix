0ae59b1de6d898532e88f4e70b0410b3a2865bb0
ikrsx <krishal.sh@tuta.io>
2025-10-19 01:53:46 +0530

    testing stylix

--- systems/iota/categories/stylix.nix
+++ systems/iota/categories/stylix.nix
@@ -3,6 +3,10 @@
 {
   stylix = {
     enable = true;
+    autoEnable = true;
+    targets = {
+      gtk.enable = true;
+    };
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
   };
 }
