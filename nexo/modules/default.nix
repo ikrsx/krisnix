5d5076c7cc8578bdfb278a283563458288aecf09
ikrsx <krishal.sh@tuta.io>
2026-09-06 22:44:35 +0530

    add niri window manager

--- nexo/modules/default.nix
+++ nexo/modules/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./boot.nix
     ./networking.nix
+    ./niri.nix
     ./system-packages.nix
   ];
 }
---
+++ nexo/modules/niri.nix
@@ -0,0 +1,15 @@
+{ config, ... }:
+
+{
+  programs.niri.enable = true;
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${config.programs.niri.package}/bin/niri-session";
+        user = "ikrsx";
+      };
+    };
+  };
+}
